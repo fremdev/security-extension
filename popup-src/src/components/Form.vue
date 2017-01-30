c79d300562895b04494ae734788729a597f7e525
Alex Efremov <alex.efremov.dev@gmail.com>
2017-01-30 20:25:02 +0200

    Add data binding in Form, simple onSubmit function

--- popup-src/src/components/Form.vue
+++ popup-src/src/components/Form.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="form">
-    <form>
+    <form @submit.prevent="onSubmit">
       <div class="form__input">
         <label>
-          Nickname:
-          <input type="text">
+          Username:
+          <input type="text" v-model="username">
         </label>
       </div>
       <div class="form__input">
         <label>
           Password:
-          <input type="password">
+          <input type="password" v-model="password">
         </label>
       </div>
+      <button type="submit">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log('Works:' + this.username  + 'pass' + this.password);
+    }
+  }
 }
 </script>
 
