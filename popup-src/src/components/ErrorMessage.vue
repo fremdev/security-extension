834420cf967050df9e04dbfd093fab4d3b97a42f
Alex Efremov <alex.efremov.dev@gmail.com>
2017-01-30 21:50:08 +0200

    Add onSubmit with axios, add ErrorMessage component

---
+++ popup-src/src/components/ErrorMessage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="error-message">
+    <h3>Error:</h3><p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .error-message {
+    background-color: #ffeeee;
+    color: #ff3333;
+    padding: 5px 15px;
+    margin-bottom: 20px;
+  }
+  h3 {
+    padding: 0 10px;
+  }
+</style>
--- popup-src/src/components/Form.vue
+++ popup-src/src/components/Form.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="form">
+    <error-message v-if="errorMessage" :message="errorMessage" />
     <form @submit.prevent="onSubmit">
       <div class="form__input">
         <label>
@@ -19,23 +20,49 @@
 </template>
 
 <script>
+import axios from 'axios';
+import ErrorMessage from './ErrorMessage.vue';
+
 export default {
+  name: 'app-form',
+  components: {
+    ErrorMessage,
+  },
   data() {
     return {
       username: '',
       password: '',
+      errorMessage: '',
     };
   },
   methods: {
     onSubmit() {
-      console.log('Works:' + this.username  + 'pass' + this.password);
+      const url = 'https://www.ovpn.se/v2/api/client';
+      axios.post(url, {
+        username: this.username,
+        password: this.password
+      })
+      .then((res) => {
+        if(res.status === 200) {
+          console.log(res.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+        if (error.response.status === 422) {
+          this.errorMessage = error.response.data.message;
+        }
+      });
     }
-  }
+  },
 }
 </script>
 
 <style>
 .form__input {
   margin-bottom: 15px;
+}
+.error {
+  background-color: #
 }
 </style>
