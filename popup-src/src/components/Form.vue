dc5f211763047542bd9fe4077b00963ae889e6f1
Alex Efremov <alex.efremov.dev@gmail.com>
2017-01-30 20:15:03 +0200

    Add basic Form component

---
+++ popup-src/src/components/Form.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form">
+    <form>
+      <div class="form__input">
+        <label>
+          Nickname:
+          <input type="text">
+        </label>
+      </div>
+      <div class="form__input">
+        <label>
+          Password:
+          <input type="password">
+        </label>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.form__input {
+  margin-bottom: 15px;
+}
+</style>
